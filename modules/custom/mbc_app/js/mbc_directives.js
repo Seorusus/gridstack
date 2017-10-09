3e68b3b6cb7d6c9436241a8c6ab32ef5a4d4fec2
Aleksey Babko <inzorcom@gmail.com>
2017-10-09 10:24:22 +0300

    593 fixed TypeError: element.datepicker is not a function

--- modules/custom/mbc_app/js/mbc_directives.js
+++ modules/custom/mbc_app/js/mbc_directives.js
@@ -99,15 +99,17 @@
                             mbcWidgetContent = '<button type="button" class="btn btn-default" ng-model="button.toggle" bs-checkbox>Button</button>';
                             break;
                         case 'calendar':
-                            var myDatepicker = element.datepicker({
-                                onSelect:function (dateText, inst) {
-                                    scope.$apply(function(scope){
-                                        // Change binded variable
-                                        ngModel.assign(scope, dateText);
-                                    });
+                            var calElement = '<input type="text" class="form-control" ng-model="selectedDate" name="date" bs-datepicker>';
+                            $(calElement).datepicker({
+                                inline: true,
+                                dateFormat: 'dd.mm.yy',
+                                onSelect: function(dateText) {
+                                    var modelPath = $(this).attr('ng-model');
+                                    putObject(modelPath, scope, dateText);
+                                    scope.$apply();
                                 }
                             });
-                            mbcWidgetContent = '<div class="calendar">' + myDatepicker + '</div>';
+                            mbcWidgetContent = calElement;
                             break;
                         default:
                             mbcWidgetContent = 'in proccess';
