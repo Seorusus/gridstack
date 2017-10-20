2d040fb5399cb8608a15fdec4484af7670b22358
Aleksey Babko <inzorcom@gmail.com>
2017-10-20 09:17:01 +0300

    593 minor fixes

--- modules/custom/mbc_app/js/mbc_app.js
+++ modules/custom/mbc_app/js/mbc_app.js
@@ -1,3 +1,7 @@
 var mbcApp = angular.module("mbcApp", ['ngSanitize', 'dndLists', 'gridstack-angular', 'mgcrea.ngStrap', 'mgcrea.ngStrap.datepicker']);
 
-var app = angular.module('gridstack-angular', ['mbcApp', 'mgcrea.ngStrap', 'mgcrea.ngStrap.datepicker']);
+var app = angular.module('gridstack-angular', ['mbcApp', 'mgcrea.ngStrap', 'mgcrea.ngStrap.datepicker']).config(function($datepickerProvider) {
+    angular.extend($datepickerProvider.defaults, {
+        autoclose: true,
+    });
+})
