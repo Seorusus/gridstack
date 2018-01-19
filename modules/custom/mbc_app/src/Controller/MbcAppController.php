fc74c512fedad07ed93f691363a3fd08f482326c
Aleksey Babko <inzorcom@gmail.com>
2018-01-19 21:37:18 +0300

    bugfixing based on user feedback (19.01.2018): added install config, recreated page template, bugfixes

--- modules/custom/mbc_app/src/Controller/MbcAppController.php
+++ modules/custom/mbc_app/src/Controller/MbcAppController.php
@@ -10,26 +10,26 @@ class MbcAppController extends ControllerBase {
       '#title' => $title,
     );
 
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/jquery';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/jquery.ui';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbcCountdown';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/videosharing';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/uiBootstrap';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/angularjs';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/angular_sanitize';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/jquery-scrollbar';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/angular_dnd_lists';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/lodash';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/gridstack';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/touchPunch';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/jquery-minicolors';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbc_app';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbc_controllers';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbc_directives';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbc_services';
-    $build['mbc_app']['#attached']['library'][] = 'mbc_app/bootstrap';
-    $build['mbc_app']['#attached']['drupalSettings']['csrf'] = \Drupal::csrfToken()->get('rest');
-    $build['mbc_app']['#attached']['drupalSettings']['baseUrl'] = $base_url;
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/jquery';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/jquery.ui';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbcCountdown';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/videosharing';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/uiBootstrap';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/angularjs';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/angular_sanitize';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/jquery-scrollbar';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/angular_dnd_lists';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/lodash';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/gridstack';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/touchPunch';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/jquery-minicolors';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbc_app';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbc_controllers';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbc_directives';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/mbc_services';
+//    $build['mbc_app']['#attached']['library'][] = 'mbc_app/bootstrap';
+//    $build['mbc_app']['#attached']['drupalSettings']['csrf'] = \Drupal::csrfToken()->get('rest');
+//    $build['mbc_app']['#attached']['drupalSettings']['baseUrl'] = $base_url;
     return $build;
   }
 }
